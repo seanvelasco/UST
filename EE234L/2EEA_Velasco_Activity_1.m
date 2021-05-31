e8f7320e113970875dd0b87cebaeeff366051242
Sean Velasco <seanvelasco@ieee.org>
2021-06-01 06:40:54 +0800

    Added intial MATLAB/Octave code for EE234L.

---
+++ EE234L/2EEA_Velasco_Activity_1.m
@@ -0,0 +1,22 @@
+R1 = 14
+R2 = 18
+R3 = 11
+R4 = 3
+
+RT = 1/(1/R1 + 1/R2 + 1/R3 + 1/R4)
+
+printf("The equivalent resistance is\n", RT)
+
+V1 = 26
+R1 = 9
+R2 = 14
+R3 = 14
+R4 = 1
+
+RT = R1 + [1/(1/R2 + 1/R3 + 1/R4)]
+
+IT = V1/RT
+
+printf("The total current across the circuit is is\n", RT)
+
+
